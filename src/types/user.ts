0930daa3f6c3f78ba8b8b27a1cb12082d6d46b17
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-05-19 11:43:15 +0300

    feat: add user type

---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: string;
+    userName: string;
+    email: string;
+    firstName: string;
+    lastname: string;
+    patronymic: string | null | undefined;
+    isVip: boolean;
+    permissions: string[];
+}

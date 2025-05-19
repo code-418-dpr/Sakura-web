6089b90c3f040e8f321f55ce71e4771cd4b76c1b
AntonKorzhov <mr.demonsmert@mail.ru>
2025-05-19 17:00:08 +0300

    Add Catalog

--- src/app/catalog/_components/search-form.tsx
+++ src/app/catalog/_components/search-form.tsx
@@ -2,11 +2,35 @@
 
 import React, { useState } from "react";
 
-import { Button, DateRangePicker, Form, Input } from "@heroui/react";
+import {
+    Button,
+    Checkbox,
+    DateRangePicker,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownTrigger,
+    Form,
+    Input,
+    NumberInput,
+} from "@heroui/react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import type { Selection } from "@react-types/shared";
 
+interface AwardItem {
+    key: string;
+    name: string;
+}
+
+const AWARDS: AwardItem[] = [
+    { key: "DEFAULT", name: "Любой" },
+    { key: "MONEY", name: "Денежный приз" },
+    { key: "BONUS", name: "Бонусные баллы" },
+    { key: "PRODUCT", name: "Товары" },
+];
 export function SearchForm() {
     const [query, setQuery] = useState("");
-
+    const [selectedAward, setSelectedAward] = React.useState<string>();
     const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = (e: React.FormEvent) => {
@@ -16,39 +40,39 @@ export function SearchForm() {
         setIsLoading(false);
     };
 
-    // const renderDropdown = (
-    //     label: string,
-    //     items: { key: string; name: string }[],
-    //     selectedKey: string | undefined,
-    //     onSelectionChange: (keys: Selection) => void,
-    //     allowEmpty = false,
-    // ) => {
-    //     const selectedName = items.find((i) => i.key === selectedKey)?.name ?? "Выберите...";
+    const renderDropdown = (
+        label: string,
+        items: AwardItem[],
+        selectedKey: string | undefined,
+        onSelectionChange: (keys: Selection) => void,
+        allowEmpty = false,
+    ) => {
+        const selectedName = items.find((i) => i.key === selectedKey)?.name ?? AWARDS[0].name;
+        if (selectedName === "Любой") selectedKey = AWARDS[0].key;
 
-    //     return (
-    //         <div className="mb-4">
-    //             <label className="mb-2 block text-sm font-medium">{label}</label>
-    //             <Dropdown className="w-full">
-    //                 <DropdownTrigger>
-    //                     <Button variant="bordered" className="w-full justify-between text-left">
-    //                         {selectedName}
-    //                     </Button>
-    //                 </DropdownTrigger>
-    //                 <DropdownMenu
-    //                     disallowEmptySelection={!allowEmpty}
-    //                     selectionMode="single"
-    //                     selectedKeys={selectedKey ? new Set([selectedKey]) : new Set()}
-    //                     onSelectionChange={onSelectionChange}
-    //                 >
-    //                     {items.map((item) => (
-    //                         <DropdownItem key={item.key}>{item.name}</DropdownItem>
-    //                     ))}
-    //                 </DropdownMenu>
-    //             </Dropdown>
-    //         </div>
-    //     );
-    // };
-
+        return (
+            <div className="mb-4">
+                <label className="mb-2 block translate-x-2 text-sm font-medium">{label}</label>
+                <Dropdown className="w-full">
+                    <DropdownTrigger>
+                        <Button variant="bordered" className="w-full justify-between text-left">
+                            {selectedName}
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                        disallowEmptySelection={!allowEmpty}
+                        selectionMode="single"
+                        selectedKeys={selectedKey ? new Set([selectedKey]) : new Set()}
+                        onSelectionChange={onSelectionChange}
+                    >
+                        {items.map((item) => (
+                            <DropdownItem key={item.key}>{item.name}</DropdownItem>
+                        ))}
+                    </DropdownMenu>
+                </Dropdown>
+            </div>
+        );
+    };
     return (
         <Form onSubmit={handleSubmit}>
             <div className="grid w-full grid-cols-1 gap-4">
@@ -65,13 +89,37 @@ export function SearchForm() {
                 </div>
 
                 <div className="mb-4">
-                    <label className="mb-2 block text-sm font-medium">Интервал подачи</label>
+                    <label className="mb-2 block translate-x-2 text-sm font-medium">Интервал проведения</label>
                     <DateRangePicker className="w-full" />
                 </div>
+                {renderDropdown(
+                    "Призовой фонд",
+                    AWARDS,
+                    selectedAward,
+                    (keys: Selection) => {
+                        const selectedKey = Array.from(keys)[0] as string | undefined;
+                        setSelectedAward(selectedKey);
+                    },
+                    false,
+                )}
+                <div>
+                    <p className="translate-x-2 text-sm font-medium">Стоимость</p>
+                    <div className="mt-4 flex flex-row gap-4">
+                        <NumberInput className="" placeholder="Минимальная" defaultValue={0} />
+                        <p className="flex items-center text-2xl font-bold">-</p>
+                        <NumberInput className="" placeholder="Максимальная" defaultValue={100000} />
+                    </div>
+                </div>
+                <Checkbox defaultSelected className="translate-x-2">
+                    VIP
+                </Checkbox>
             </div>
-            <div className="flex justify-end gap-2">
-                <Button variant="flat">Сбросить</Button>
-                <Button type="submit" isLoading={isLoading}>
+
+            <div className="mt-6 flex justify-end gap-2">
+                <Button variant="flat" type="reset" className="mr-auto">
+                    Сбросить
+                </Button>
+                <Button type="submit" isLoading={isLoading} startContent={<Icon icon="iconoir:search" />}>
                     Поиск
                 </Button>
             </div>
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -10,15 +10,19 @@ import { Tab } from "@/types/tabs";
 
 import { SearchForm } from "./_components/search-form";
 
+// import { MainCards } from "@/components/main-cards";
+// import { Lottery } from "@/types/lottery";
+
 export default function CatalogPage() {
     //const { isOpen, onOpen, onOpenChange } = useDisclosure();
     const [activeTab, setActiveTab] = useState<Tab>("main");
+    //const [isLotteryLoading, setIsLotteryLoading] = useState(false);
     // const { user } = useContext(AuthContext)!;
 
     return (
         <div>
             <NavbarElement activeTab={activeTab} setActiveTabAction={setActiveTab} />
-            <div className="container mx-auto py-6">
+            <div className="mx-auto min-h-[80vh] px-6 py-6">
                 <SearchCardOrDrawer>
                     {" "}
                     <SearchForm />

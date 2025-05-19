1d7a2fa24c55463a533c5d600224042db69a3adf
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-05-19 16:31:38 +0300

    feat: first version of admin page

---
+++ src/app/admin/_components/lotteries-tab.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { useEffect, useMemo, useState } from "react";
+
+import LotteriesCards from "@/app/admin/_components/lotteries-cards";
+import { MainCards } from "@/components/main-cards";
+import { LotteryItem } from "@/types/LotteryItem";
+
+interface Paged<T> {
+    items: T[];
+    pagination: {
+        page: number;
+        pageSize: number;
+        totalItems: number;
+        totalPages: number;
+    };
+}
+
+export default function LotteriesTab() {
+    const [isLoading] = useState(false);
+    const [lotteriesData, setLotteriesData] = useState<Paged<LotteryItem> | null>(null);
+    const [page, setPage] = useState(1);
+
+    const perPage = 8;
+
+    useEffect(() => {
+        setLotteriesData({
+            items: Array.from({ length: 14 }, (_, index) => ({
+                id: `id-${index}`,
+                name: `Лотерея-${index + 1}`,
+                image: `https://avatars.mds.yandex.net/i?id=2a00000196d3064f672cf2fbc01d4b2a4812-1344766-fast-images&n=13`,
+                age: `${index + 1} лет 2 месяца`,
+                isVaccinated: index % 2 === 0,
+                humanAttitude: "Хорошее",
+                animalsAttitude: "Хорошее",
+                startDate: "14.05.2025",
+                endDate: "18.05.2025",
+                isVirtual: index % 2 === 0,
+                prize: 140000,
+            })),
+            pagination: {
+                page,
+                pageSize: perPage,
+                totalItems: 14,
+                totalPages: 2,
+            },
+        });
+    }, [page]);
+
+    const animalsPageItems = useMemo(() => {
+        if (!lotteriesData) return [];
+        const start = (page - 1) * lotteriesData.pagination.pageSize;
+        const end = start + lotteriesData.pagination.pageSize;
+
+        return lotteriesData.items.slice(start, end);
+    }, [page, lotteriesData]);
+
+    const totalAnimalsPages = lotteriesData?.pagination.totalPages ?? 1;
+
+    return (
+        <>
+            <div className="flex min-h-[100vh] w-full">
+                <div className="flex-1 p-4">
+                    <MainCards<LotteryItem>
+                        isLoading={isLoading}
+                        pageItems={animalsPageItems}
+                        totalPages={totalAnimalsPages}
+                        page={page}
+                        setPageAction={setPage}
+                        renderCardsAction={(items) => <LotteriesCards paginatedData={items} />}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}

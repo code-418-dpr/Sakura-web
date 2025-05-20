4ec86941d3b900c53ef3b32b418c64c62f0c747c
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-05-20 16:15:07 +0300

    refactor: land route

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 
 import { signOut } from "next-auth/react";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { useAuth } from "@/hooks/use-auth";
 import { PageTab } from "@/types/tabs";
@@ -44,7 +44,6 @@ export default function NavbarElement({ activeTab, setActiveTabAction }: NavbarP
     const { isOpen: isBalanceOpen, onOpen: onBalanceOpen, onOpenChange: onBalanceOpenChange } = useDisclosure();
     const { user, isLoading, isAuthenticated } = useAuth();
     const router = useRouter();
-    const pathname = usePathname();
 
     const handleNavigation = (e: PressEvent, tab: PageTab) => {
         setActiveTabAction(tab);
@@ -71,23 +70,18 @@ export default function NavbarElement({ activeTab, setActiveTabAction }: NavbarP
         <>
             <Navbar>
                 <NavbarBrand className="gap-x-4">
-                    <Image alt="Sakura Logo" src="sakura.png" width={32} height={32} />
-                    <p className="font-bold text-inherit">SAKURA</p>
+                    <Link
+                        color="foreground"
+                        href="/"
+                        onPressEnd={(e: PressEvent) => {
+                            handleNavigation(e, "main");
+                        }}
+                    >
+                        <Image alt="Sakura Logo" src="sakura.png" width={32} height={32} />
+                        <p className="font-bold text-inherit">SAKURA</p>
+                    </Link>
                 </NavbarBrand>
                 <NavbarContent className="hidden gap-4 sm:flex" justify="center">
-                    {pathname !== "/" && (
-                        <NavbarItem key="main">
-                            <Link
-                                color="foreground"
-                                href="/"
-                                onPressEnd={(e: PressEvent) => {
-                                    handleNavigation(e, "main");
-                                }}
-                            >
-                                Главная
-                            </Link>
-                        </NavbarItem>
-                    )}
                     {tabs.map((tab) => (
                         <NavbarItem key={tab}>
                             <Link
